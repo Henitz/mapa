e2b4d78b7975e37589c291fe87df2be44a45ea4f
Henitz <henitz@gmail.com>
2022-09-04 19:24:58 -0300

    aplicacao funcional no componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapaimoveisComponent } from './mapaimoveis/mapaimoveis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: 'mapa', pathMatch: 'full',},
+{path: 'mapa', component: MapaimoveisComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
